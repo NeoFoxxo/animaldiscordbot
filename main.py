93da13317e0897b8c74f9dc3979a3ed3ae5c3387
unknown <hamish@cits.co.nz>
2022-07-09 15:50:08 +1000

    bot code

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import urllib3
 import re
 import praw
 from discord.ext import commands
+
 #important codes
 with open('disc-token.txt', 'r') as f:
     tokendiscord = f.read()
@@ -26,11 +27,16 @@ reddit = praw.Reddit(
 TOKEN = tokendiscord
 
 client = discord.Client()
-depss = random.randint(0, 50)
+
+gallery = ('https://www.reddit.com/gallery/')
+video = ('https://v.redd.it/')
+
+#notification that the bot is online
 @client.event
 async def on_ready():
     print('we have logged in as {0.user}'.format(client))
 
+#relay all messages to console
 @client.event
 async def on_message(message):
     username = str(message.author).split('#')[0]
@@ -40,7 +46,8 @@ async def on_message(message):
 
     if message.author == client.user:
         return
-
+    
+    #only sends messages in general channel
     if message.channel.name == 'general':
         if user_message.lower() == 'hello':
             await message.channel.send(f'wasup {username}!')
@@ -51,28 +58,38 @@ async def on_message(message):
             sub = reddit.subreddit('rarepuppers')
             posts = [post for post in sub.hot(limit=250)]
             random_post = random.choice(posts)
-            #random_post_number = random.choice(range (0, 250))
-            #random_post = posts[random_post_number]
-            if random_post.is_video:
-              random_post = random.choice(posts)
-              #random_post_number = random.choice(range (0, 250))
-              #random_post = posts[random_post_number]
-              await message1.delete()
-              await message2.delete()
-              await message.channel.send(random_post.url)
-              return
-            elif depss == 14:
+            #funny easter egg
+            depss = random.randint(0, 14)
+            if depss == 14:
                 await message1.delete()
                 await message2.delete()
                 await message.channel.send(f'https://cdn.discordapp.com/attachments/461799833540231170/994573993355001856/6m1k6j.jpg')
-            elif 'gallery_data' in random_post.__dict__:
+                return
+            while video in random_post.url:
               random_post = random.choice(posts)
-              #random_post_number = random.choice(range (0, 250))
-              #random_post = posts[random_post_number]
-              await message1.delete()
-              await message2.delete()
-              await message.channel.send(random_post.url)
-              return
+              if video not in random_post.url:
+                await message1.delete()
+                await message2.delete()
+                await message.channel.send(random_post.url)
+                break 
+            while gallery in random_post.url:
+              random_post = random.choice(posts)
+              if gallery not in random_post.url:
+                await message1.delete()
+                await message2.delete()
+                await message.channel.send(random_post.url)
+                break 
+            #else:  
+                #await message1.delete()
+                #await message2.delete()
+                #await message.channel.send(random_post.url)
+                #return
+            #elif 'gallery_data' in random_post.__dict__:
+              #random_post = random.choice(posts)
+              #await message1.delete()
+              #await message2.delete()
+              #await message.channel.send(random_post.url)
+              #return
             await message1.delete()
             await message2.delete()
             await message.channel.send(random_post.url)
@@ -129,6 +146,7 @@ async def on_message(message):
             await message2.delete()
             await message.channel.send(random_post.url)
             return
+        #find a post from a random subreddit in the list
         elif user_message.lower() == '!random':
             message1 = await message.channel.send(f'One cute animal coming up {username}!')
             message2 = await message.channel.send(f'https://user-images.githubusercontent.com/3053271/32455273-6118a5fc-c2e7-11e7-8265-9829b231be4d.gif')
@@ -137,6 +155,7 @@ async def on_message(message):
             sub = reddit.subreddit(random_sub)
             posts = [post for post in sub.hot(limit=250)]
             random_post = random.choice(posts)
+            #if post is a video post find another post
             if random_post.is_video:
               random_post = random.choice(posts)
               await message1.delete()
@@ -147,6 +166,7 @@ async def on_message(message):
                 await message1.delete()
                 await message2.delete()
                 await message.channel.send(f'https://cdn.discordapp.com/attachments/461799833540231170/994573993355001856/6m1k6j.jpg')
+            #if post is a gallery post find another post
             elif 'gallery_data' in random_post.__dict__:
               random_post = random.choice(posts)
               await message1.delete()
@@ -157,24 +177,19 @@ async def on_message(message):
             await message2.delete()
             await message.channel.send(random_post.url)
             return
+        #troll command lol
         elif user_message.lower() == '!horse':
             await message.channel.send(f'no horses for you {username}!')
             return
+        #give a list of all commands
+        elif message.content.startswith('!help'):
+            embedvar=discord.Embed(title="Commands", description="Here are the all commands and what they do", color=discord.Color.blue())
+            embedvar.add_field(name="**!dog**", value="Get a photo of a cute doggo!", inline=False)
+            embedvar.add_field(name="**!cat**", value="Get a photo of a cute cat!", inline=False)
+            embedvar.add_field(name="**!bunny**", value="Get a photo of a cute bunny!", inline=False)
+            embedvar.add_field(name="**!horse**", value="Get a photo of a cute horse!", inline=False)
+            embedvar.add_field(name="**!random**", value="Get a photo of a random animal!", inline=False)
+            await message.channel.send(embed=embedvar)
+            return     
 
-        #elif user_message.lower() == '!help':
-            async def embed(ctx):
-                embed=discord.Embed(
-                title="Commands",
-                    description="Here are the all commands and what they do",
-                    color=discord.Color.blue())
-                embed.add_field(name="**!dog**", value="Get a photo of a cute doggo!", inline=False)
-                embed.add_field(name="**!cat**", value="Get a photo of a cute cat!", inline=False)
-                embed.add_field(name="**!bunny**", value="Get a photo of a cute bunny!", inline=False)
-                embed.add_field(name="**!horse**", value="Get a photo of a cute horse!", inline=False)
-                embed.add_field(name="**!random**", value="Get a photo of a random animal!", inline=False)
-                await ctx.send(embed=embed)
-                return
-        elif user_message.lower() == '!guide':
-            await message.channel.send(f'**!dog**: Get a photo of a cute doggo! \n**!cat**: Get a photo of a cute cat! \n**!bunny**: Get a photo of a cute bunny! \n**!horse**: Get a photo of a cute horse! \n**!random**: Get a photo of a random animal!')
-            return      
 client.run(TOKEN)
